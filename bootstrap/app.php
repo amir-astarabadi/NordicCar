3c1616e4f5109abd583419152ed8f4cca0a06409
Amir Pourastarabadi <amir.astarabadi.dev@gmail.com>
2025-01-07 13:54:42 +0330

    install laravel sanctum
    add admin routes to app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,26 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        then: function () {
+            Route::middleware(['auth:sanctum', 'ability:manage-resources'])
+                ->name('api.admin.')
+                ->prefix('api/admin')
+                ->group(base_path(('routes/admin.php')));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
