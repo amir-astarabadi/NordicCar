a3c52f592552489bb240e7892ec2dc6d49f3c381
Amir Pourastarabadi <amir.astarabadi.dev@gmail.com>
2025-01-07 13:55:48 +0330

    add product creation test

---
+++ tests/Feature/Controllers/Admin/ProductController/StoreProductTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Controllers\Admin\ProductController;
+
+use App\Models\Product;
+use Tests\TestCase;
+
+class StoreProductTest extends TestCase
+{
+    private array $requestData = [];
+
+    public function setUp():void{
+        parent::setUp();
+        $this->loginAsAdmin();
+        $this->requestData = Product::factory()->make()->toArray();
+    }
+ 
+    public function test_admin_can_store_product(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->postJson(route('api.admin.products.store'), $this->requestData);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseCount('products', 1);
+
+        $this->assertDatabaseHas('products', $this->requestData);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseTransactions;
+
+    protected ?User $admin = null;
+
+    protected function loginAsAdmin()
+    {
+        $this->admin = User::factory()->create();
+        Sanctum::actingAs($this->admin, ['view-dashboard', 'manage-resources']);
+    }
 }
